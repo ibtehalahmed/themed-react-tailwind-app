9d871bb984b4e040e0f4f128535b1535dadac9f1
ibtehalahmed <ibtehal599@gmail.com>
2024-05-31 00:24:14 -0700

    config

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -1,5 +1,19 @@
 import React, { createContext, useState, ReactNode, FC } from 'react';
+// Defines the color channels. Passed to the context from each app.
+// i.e. {"rgb-accent-1": "252 66 201"}
+export interface IThemeRGB {
+  "rgb-accent-1"?: string;
+}
 
+// Name of the CSS variables used in tailwind.config.ts
+export interface IThemeVariables {
+  "--color-accent-1": string;
+}
+
+// Name of the defined colors in the Tailwind theme
+export interface IThemeColors {
+  "accent-1"?: string;
+}
 interface ThemeSettings {
   primaryColor: string;
   secondaryColor: string;
